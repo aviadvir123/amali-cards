91389dd925aa977f557c03c668fbeaf484619cca
Avia Dvir <avia.dvir@dash0.com>
2026-03-15 23:10:25 +0200

    Add gift card DANIELH100

--- docs/app.js
+++ docs/app.js
@@ -26,13 +26,14 @@ var LOCALSTORAGE_KEY = "amali_punch_card";
 // ============================================================
 
 var PROMO_CODES = {
-  "JOY50":    50,
-  "WARM80":   80,
-  "SMILE100": 100,
-  "TREAT120": 120,
-  "SHINE150": 150,
-  "CHEER180": 180,
-  "GOLD200":  200
+  "JOY50":      50,
+  "WARM80":     80,
+  "SMILE100":   100,
+  "TREAT120":   120,
+  "SHINE150":   150,
+  "CHEER180":   180,
+  "GOLD200":    200,
+  "DANIELH100": 100
 };
 
 // The localStorage key used to track redeemed promo codes.

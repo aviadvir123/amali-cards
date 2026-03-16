84bc8f338e20430b32a6ca505e64d5ee16f3f82b
aviadvir123 <avia.dvir.123@gmail.com>
2026-03-16 20:28:48 +0200

    Update promo codes via admin

--- docs/app.js
+++ docs/app.js
@@ -26,7 +26,6 @@ var LOCALSTORAGE_KEY = "amali_punch_card";
 // ============================================================
 
 var PROMO_CODES = {
-  "WARM80": 80,
   "SMILE100": 100,
   "TREAT120": 120,
   "SHINE150": 150,
